8fc545adb1d11175fc338f6c286ae84870a7be57
Thúlio Costa <thulio@thuliocosta.com>
2016-08-02 11:09:46 -0300

    Add lein profiles

---
+++ clojure/profiles.clj
@@ -0,0 +1,7 @@
+{:user  {:plugins  [[venantius/ultra "0.4.1"]
+                    [lein-auto "0.1.2"]
+                    [lein-kibit "0.1.2"]
+                    [lein-ancient "0.6.10"]
+                    [lein-cloverage "1.0.6"]
+                    ]}}
+
