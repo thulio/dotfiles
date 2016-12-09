4abdf571957ce89a6ae331d610ab951a2af1e399
Thúlio Costa <thulio@thuliocosta.com>
2016-12-09 11:37:31 -0200

    Add proto-repl and nightlight

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -3,5 +3,6 @@
                     [lein-kibit "0.1.2"]
                     [lein-ancient "0.6.10"]
                     [lein-cloverage "1.0.6"]
-                    ]}}
+                    [proto-repl "0.3.1"]
+                    [nightlight/lein-nightlight "1.3.0"]]}}
 
