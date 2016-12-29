3477b664aef24f2b160168fc438c6d23576ad580
Thúlio Costa <thulio@thuliocosta.com>
2016-12-29 08:02:58 -0200

    Add lein-virgil

--- clojure/profiles.clj
+++ clojure/profiles.clj
@@ -4,5 +4,7 @@
                     [lein-ancient "0.6.10"]
                     [lein-cloverage "1.0.6"]
                     [proto-repl "0.3.1"]
-                    [nightlight/lein-nightlight "1.3.0"]]}}
+                    [nightlight/lein-nightlight "1.3.0"]
+                    [lein-virgil "0.1.4"]]}}
+
 
