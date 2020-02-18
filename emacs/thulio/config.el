2a73df708a4d145866d1b2377498bdcf0dd587c6
Thúlio Costa <thulio@thuliocosta.com>
2020-02-18 13:49:38 -0300

    chore: Remove emacs stuff

--- emacs/thulio/config.el
+++
@@ -1,8 +0,0 @@
-(add-hook 'elixir-format-hook '(lambda ()
-                                 (if (projectile-project-p)
-                                   (setq elixir-format-arguments (list "--dot-formatter" (concat (projectile-project-root) "/.formatter.exs")))
-                                   (setq elixir-format-arguments nil))))
-
-(add-hook 'elixir-mode-hook
-          (lambda () (add-hook 'before-save-hook 'elixir-format-before-save)))
-
